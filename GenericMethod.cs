250b8dfd3268e18b907773404c225891bb6c219e
Gofrieza <93874332+Gofrieza@users.noreply.github.com>
2022-02-18 15:00:28 +0530

    Add files via upload

---
+++ GenericMethod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDemo
+{
+    class GenericMethod
+    {
+        public static void Display<T>(T[] test)
+        {
+            for (int i = 0; i < test.Length; i++)
+            {
+                Console.Write(test[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,26 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using FindMaximumNumber;
+using GenericDemo;
+using static GenericDemo.GenericMethod;
 
-//Console.WriteLine("Hello, World!");
-
-//Console.WriteLine("Find max Number");
-//int output = MaximumNumberCheck.MaximumIntegerNumber(11, 22, 33);
-//Console.WriteLine(output);
-//double doubleoutput = MaximumNumberCheck.MaximumFloatNumber(111.2, 22.45, 78.56);
-//Console.WriteLine(doubleoutput);
-//string strigoutput = MaximumNumberCheck.MaximumStringNumber("22", "33", "44");
-//Console.WriteLine(strigoutput);
-
-int[] intArray = { 112, 344, 432, 555, 678 };
-GenericMaximum<int> generic = new GenericMaximum<int>(intArray);
-generic.PrintMaxValue();
-double[] doubleArray = { 11.2, 34.4, 4.32, 55.5, 6.78 };
-GenericMaximum<double> genericDouble = new GenericMaximum<double>(doubleArray);
-genericDouble.PrintMaxValue();
-string[] stringArray = { "111", "222", "333", "55", "999" };
-GenericMaximum<string> genericString = new GenericMaximum<string>(stringArray);
-//GenericMaximum<Employee> genericString = new GenericMaximum<string>(stringArray);
-genericString.PrintMaxValue();
+    //Console.WriteLine("Hello World!");
+int[] intArray = { 2, 4, 5, 6, 8 };
+char[] charArray = { 'H', 'E', 'L', 'L', 'O' };
+double[] doubleArray = { 1.1, 3.3, 5.5, 8.7 };
+GenericMethod.Display<int>(intArray);
+GenericMethod.Display<char>(charArray);
+GenericMethod.Display<double>(doubleArray);
 Console.ReadKey();
-
-
